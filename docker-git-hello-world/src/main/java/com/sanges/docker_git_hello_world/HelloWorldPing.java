fe98ce41e17aee38941b6499e7943a3cc8532a1e
f-sanges <francesco.sanges@gmail.com>
2020-12-16 00:22:17 +0100

    Modified HelloWorldPing java class

--- docker-git-hello-world/src/main/java/com/sanges/docker_git_hello_world/HelloWorldPing.java
+++ docker-git-hello-world/src/main/java/com/sanges/docker_git_hello_world/HelloWorldPing.java
@@ -7,7 +7,7 @@ package com.sanges.docker_git_hello_world;
 public class HelloWorldPing 
 {
 	public static void main(String args[]) throws Exception{
-		for(int i = 0 ; i < 10; i++){
+		for(int i = 0 ; i < 50; i++){
 			System.out.println("Hello World Ping " + i );
 			Thread.sleep(1000);
 		}
